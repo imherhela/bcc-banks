e07385223003b754312b599cd135e9da2892e650
ImHerHela <76200467+imherhela@users.noreply.github.com>
2026-03-30 21:43:19 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 game 'rdr3'
 lua54 'yes'
-version '1.4.1'
+version '1.5.0'
 author 'BCC Scripts'
 
 client_scripts {
